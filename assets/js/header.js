59c7c6ea185a3bbeb22cc10fc65913e2994f74fd
speedup-guru <paul@speedup.guru>
2025-08-08 15:33:41 +0400

    update

--- assets/js/header.js
+++ assets/js/header.js
@@ -14,16 +14,16 @@ const QureMainMenu = {
     },
 
     setHeaderHeights: function () {
-        const announcementBar = document.getElementById("announcement-bar");
-        const pageHeader = document.getElementById("page-header");
+        //const announcementBar = document.getElementById("announcement-bar");
+        //const pageHeader = document.getElementById("page-header");
         const stickyCart = document.querySelector('.qure__sticky-atc');
 
-        const d = announcementBar ? announcementBar.clientHeight : 0;
-        const p = pageHeader ? pageHeader.clientHeight : 0;
+        //const d = announcementBar ? announcementBar.clientHeight : 0;
+        //const p = pageHeader ? pageHeader.clientHeight : 0;
         const s = stickyCart ? stickyCart.clientHeight : 0;
 
-        document.body.style.setProperty("--height-bar", `${d}px`);
-        document.body.style.setProperty("--height-header", `${p}px`);
+        //document.body.style.setProperty("--height-bar", `${d}px`);
+        //document.body.style.setProperty("--height-header", `${p}px`);
         document.body.style.setProperty("--sticky-cart", `${s}px`);
     }
 };
